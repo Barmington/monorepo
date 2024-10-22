2fd698fd344aa30cac5719d107406a307f566c37
Barmington Chuku <barmingtonk@gmail.com>
2024-10-22 02:19:52 +0100

    init bruv

---
+++ server/server.js
@@ -0,0 +1,14 @@
+import express from "express";
+import cors from "cors";
+
+const app = express();
+
+app.use(cors);
+
+app.get("/message", function (request, response) {
+  response.json("{wow, nice project!}");
+});
+
+app.listen(3000, function () {
+  console.log("Running on port 3000");
+});
