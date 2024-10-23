2a59a875903455d1dbbebfcada543f985f3fe7ea
Barmington Chuku <barmingtonk@gmail.com>
2024-10-23 11:01:20 +0100

    deploy update

--- server/server.js
+++ server/server.js
@@ -3,10 +3,10 @@ import cors from "cors";
 
 const app = express();
 
-app.use(cors);
+app.use(cors());
 
 app.get("/message", function (request, response) {
-  response.json("{wow, nice project!}");
+  response.json("wow, nice project!");
 });
 
 app.listen(3000, function () {
